df316ef44913aed00481f10a9f90064b396a0679
hez <146683605+hezzexx@users.noreply.github.com>
2024-07-19 22:55:34 -0700

    Create 13987158269.lua

---
+++ Games/13987158269.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 13987158269 then
+end
+    while true do
+        local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+local location = CFrame.new(-738, 73, -549)
+pl.CFrame = location
+wait(4)
+    end
