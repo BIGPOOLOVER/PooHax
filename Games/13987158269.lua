f2b338ca97d7f1e0b7c5a5f452976ee97e2a88f2
hez <146683605+hezzexx@users.noreply.github.com>
2024-07-20 07:09:44 -0700

    Update 13987158269.lua

--- Games/13987158269.lua
+++ Games/13987158269.lua
@@ -1,4 +1,41 @@
--- god its finally fixed
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "PooHax",
+   LoadingTitle = "PooHax",
+   LoadingSubtitle = "'brah wtf brah' - big poo",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, 
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", 
+      RememberJoins = true
+   },
+   KeySystem = false, 
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true, 
+      GrabKeyFromSite = false, 
+      Key = {"Hello"} 
+   }
+})
+
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
+local Section = Tab:CreateSection("Farms")
+Section:Set("Farms")
+
+local Toggle = Tab:CreateToggle({
+   Name = "Auto-Farm (Glass Bridge)",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(GlassBridgeFarm)
+   local GlassBridge
 if game.PlaceId == 13987158269 then
 end
     while true do
@@ -6,4 +43,11 @@ end
 local location = CFrame.new(-738, 73, -549)
 pl.CFrame = location
 wait(4)
+    endFarm = true
     end
+
+
+   end,
+})
+
+Toggle:Set(false)
