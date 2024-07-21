c2323456eba4b68908ffc60f866a6a67e5f18e05
hez <146683605+hezzexx@users.noreply.github.com>
2024-07-20 19:31:01 -0700

    Update 13987158269.lua

--- Games/13987158269.lua
+++ Games/13987158269.lua
@@ -1,7 +1,4 @@
--- god its finally fixed
-if game.PlaceId == 13987158269 then
-end
-    while true do
+while true do
         local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
 local location = CFrame.new(-738, 73, -549)
 pl.CFrame = location
