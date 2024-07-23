7bc85a8f6d1bf9c8fd4cfbc73357335d5c58c144
hez <146683605+hezzexx@users.noreply.github.com>
2024-07-22 17:42:53 -0700

    Update 13987158269.lua

--- Games/13987158269.lua
+++ Games/13987158269.lua
@@ -1,3 +1,4 @@
+-- looking for the actual PooHax code?: https://github.com/hezzexx/Actually-PooHax
 while true do
         local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
 local location = CFrame.new(-738, 73, -549)
