7b827b52db7c985b7cfef405c25de1cacdbc7852
hez <146683605+hezzexx@users.noreply.github.com>
2024-07-22 18:14:34 -0700

    Update 13987158269.lua

--- Games/13987158269.lua
+++ Games/13987158269.lua
@@ -1,7 +1,85 @@
 -- looking for the actual PooHax code?: https://github.com/hezzexx/Actually-PooHax
-while true do
-        local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
-local location = CFrame.new(-738, 73, -549)
-pl.CFrame = location
-wait(4)
-    end
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "PooHax | Impossible Squid Game! Glass Bridge!",
+   LoadingTitle = "PooHax",
+   LoadingSubtitle = "'brah wtf brah' - big poo",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, 
+      FileName = "PooHax"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", 
+      RememberJoins = true
+   },
+   KeySystem = false, 
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true, 
+      GrabKeyFromSite = false, 
+      Key = {"Hello"} 
+   }
+})
+
+local Tab = Window:CreateTab("Main", 4483362458)
+local Section = Tab:CreateSection("Automation")
+
+local Toggle = Tab:CreateToggle({
+   Name = "Auto-Farm (Glass Bridge)",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+   Value = loadstring(game:HttpGet("https://raw.githubusercontent.com/hezzexx/Actually-PooHax/main/Games/Squid%20Game%20Glass%20Bridge/glassBridge.lua",true))()
+   
+   end,
+})
+
+local Section = Tab:CreateSection("Extras")
+
+local Slider = Tab:CreateSlider({
+   Name = "WalkSpeed",
+   Range = {0, 100},
+   Increment = 1,
+   Suffix = "Speed",
+   CurrentValue = 16,
+   Flag = "Slider1", 
+   Callback = function(Value)
+   while true do
+       task.wait(nil)
+	   game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
+   end
+   
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Infinite Jump",
+   Callback = function(infJump)
+   infJump = loadstring(game:HttpGet("https://raw.githubusercontent.com/hezzexx/Actually-PooHax/main/General/infJump.lua",true))()
+
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Teleport to VIP Area",
+   Callback = function(vipArea)
+   vipArea = loadstring(game:HttpGet("https://raw.githubusercontent.com/hezzexx/Actually-PooHax/main/Games/Squid%20Game%20Glass%20Bridge/vipArea.lua",true))()
+
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "Teleport to Spawn",
+   Callback = function(Spawn)
+   Spawn = loadstring(game:HttpGet("https://raw.githubusercontent.com/hezzexx/Actually-PooHax/main/Games/Squid%20Game%20Glass%20Bridge/spawn.lua",true))()
+
+   end,
+})
+
+
